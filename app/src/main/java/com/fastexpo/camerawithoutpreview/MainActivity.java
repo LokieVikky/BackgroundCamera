d5455dd5c236c758dd980f6965b1093a255dbea9
L6380321 <lokesh.n@saint-gobain.com>
2021-07-03 01:33:26 +0530

    Added Image upload feature

--- app/src/main/java/com/fastexpo/camerawithoutpreview/MainActivity.java
+++ app/src/main/java/com/fastexpo/camerawithoutpreview/MainActivity.java
@@ -1,17 +1,20 @@
 package com.fastexpo.camerawithoutpreview;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.OrientationEventListener;
 import android.view.Surface;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.AspectRatio;
@@ -23,10 +26,25 @@ import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.fastexpo.camerawithoutpreview.databinding.ActivityMainBinding;
 import com.google.common.util.concurrent.ListenableFuture;
 
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.util.concurrent.ExecutionException;
 
@@ -35,6 +53,9 @@ public class MainActivity extends AppCompatActivity {
     ActivityMainBinding mBiniding;
     ImageCapture imageCapture;
     private static final String TAG = "MainActivity";
+
+    // Server URL to POST Image
+    String baseURL = "";
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -107,7 +128,6 @@ public class MainActivity extends AppCompatActivity {
         }, ContextCompat.getMainExecutor(MainActivity.this));
     }
 
-
     void captureImage() {
         if (imageCapture == null) {
             return;
@@ -124,10 +144,11 @@ public class MainActivity extends AppCompatActivity {
                         ByteBuffer buffer = planeProxy.getBuffer();
                         byte[] bytes = new byte[buffer.remaining()];
                         buffer.get(bytes);
-
-                        // This is the captured Image
                         Bitmap bMap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                        Log.d(TAG, "onCaptureSuccess: ");
+                        String base64Image = encodeTobase64(bMap);
+                        image.close();
+                        // Call API HERE
+                        postImageToServer(base64Image);
                     }
 
                     @Override
@@ -136,5 +157,85 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
+
+    public static String encodeTobase64(Bitmap image) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte[] b = baos.toByteArray();
+        return Base64.encodeToString(b, Base64.DEFAULT);
+    }
+
+    void postImageToServer(String base64Image){
+        try {
+            JSONObject object = new JSONObject();
+            object.put("ImageBase64",base64Image);
+            httpRequest(MainActivity.this, object, Request.Method.POST, new VolleyCallback() {
+                @Override
+                public void OnSuccess(String object) {
+                    Log.d(TAG, "OnSuccess: Image Uploaded");
+                }
+
+                @Override
+                public void OnFailure(VolleyError error) {
+                    Log.d(TAG, "OnSuccess: Unable to upload Image");
+                }
+            },10000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void httpRequest(Context mContext, @Nullable JSONObject message, final int method,
+                            final VolleyCallback callBack, int timeOut) throws Exception {
+        if (mContext == null) {
+            throw new Exception("Null Context");
+        }
+        if (callBack == null) {
+            throw new Exception("Null CallBack");
+        }
+        RequestQueue requestQueue = Volley.newRequestQueue(mContext);
+        //String URL = Base_url + apiType;
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                callBack.OnSuccess(response);
+            }
+        };
+
+        Response.ErrorListener volleyErrorListener = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                callBack.OnFailure(error);
+            }
+        };
+        StringRequest stringRequest = new StringRequest(method, baseURL, responseListener, volleyErrorListener) {
+
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    return message == null ? null : message.toString().getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", message.toString(), "utf-8");
+                    return null;
+                }
+            }
+
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                String responseString = new String(response.data);
+                return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+            }
+        };
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(timeOut, 1, 1.0f));
+        requestQueue.add(stringRequest);
+    }
+
+    public interface VolleyCallback {
+        public void OnSuccess(String object);
+
+        public void OnFailure(VolleyError error);
+    }
+
 
 }
